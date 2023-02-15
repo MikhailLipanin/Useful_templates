eb7dcb60ddf7ae89bad825946941982bcabf7ae6
Mikhail Lipanin <95661085+MikhailLipanin@users.noreply.github.com>
2023-02-16 01:27:37 +0300

    Create trie.py

---
+++ python/trie.py
@@ -0,0 +1,24 @@
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.is_end_of_word = False
+
+class Trie:
+    def __init__(self):
+        self.root = TrieNode()
+
+    def insert(self, word: str) -> None:
+        current_node = self.root
+        for char in word:
+            if char not in current_node.children:
+                current_node.children[char] = TrieNode()
+            current_node = current_node.children[char]
+        current_node.is_end_of_word = True
+
+    def search(self, word: str) -> bool:
+        current_node = self.root
+        for char in word:
+            if char not in current_node.children:
+                return False
+            current_node = current_node.children[char]
+        return current_node.is_end_of_word
